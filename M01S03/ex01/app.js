146d68d70f0c64e32a55dfbc5fac933601a71b00
Fauriis <dobreflorentina58@gmail.com>
2022-05-25 13:55:37 +0300

    commitu pentru 24 MAI 2022

--- M01S03/ex01/app.js
+++ M01S03/ex01/app.js
@@ -3,3 +3,24 @@ var object = {
   surname: 'dobre',
   age: 24,
 };
+
+console.log(object.name);
+// === '24'
+console.log(object.age.toString());
+
+// Afiseaza fraza “Ma numesc xxx yyy si am xx ani.” in consola folosind operatorul de concatenare
+
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' si am ' +
+    object.age +
+    ' ani.',
+);
+
+console.warn(
+  'Afiseaza fraza “Ma numesc xxx yyy si am xx ani.” in consola folosind operatorul de concatenare',
+);
+console.log((2022 - object.age).toString());
---
+++ M01S03/ex05/app.js
@@ -0,0 +1,24 @@
+var person = {
+  name: 'Dobre',
+  surname: 'Florentina',
+  age: 24,
+  petOwner: false,
+  skills: ['html', 'javascript', 'css'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Stevan',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
