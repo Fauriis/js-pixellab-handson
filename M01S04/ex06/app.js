2f809e507a161872f33bac488c87ff9d9636972c
Fauriis <dobreflorentina58@gmail.com>
2022-05-31 22:00:46 +0300

    commit 31 mai

---
+++ M01S04/ex06/app.js
@@ -0,0 +1,34 @@
+var person = {
+  name: 'Dobre',
+  surname: 'Florentina',
+  age: 24,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
