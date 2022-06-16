6e17b633b4fce574a66e7e88d8ce9e2300210e8b
Fauriis <dobreflorentina58@gmail.com>
2022-06-16 22:03:51 +0300

    commit 16 iunie 2022

--- M01S06/ex04/app.js
+++ M01S06/ex04/app.js
@@ -1,5 +1,32 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+// Folosind exemplul de la exercitiul 2, afiseaza mesajul intr-un paragraf in loc de consola. Inainte de a adauga elementul in DOM adauga-i clasa message.
+
+(function () {
+  function showMessage(message) {
+    const paragraphElement = document.createElement('p');
+    paragraphElement.classList.add('message');
+
+    paragraphElement.innerText = message;
+
+    document.body.append(paragraphElement);
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const stage = document.querySelector('.stage');
+    // .stage ===> din CSS, se pune . inainte e selector!
+
+    // const paragraphElement = document.createElement('p');
+    // document.body.append(paragraphElement);
+
+    stage.addEventListener('mouseover', function () {
+      const message = 'Mouseul este pe scena';
+      showMessage(message);
+      console.log(message);
+    });
+
+    stage.addEventListener('mouseout', function () {
+      const message = 'Mouseul nu este pe scena';
+      showMessage(message);
+      console.log(message);
+    });
   });
 })();
