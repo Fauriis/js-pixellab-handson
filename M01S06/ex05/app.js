4982764e1442e17bd507488e103464832b73e064
Fauriis <dobreflorentina58@gmail.com>
2022-06-21 23:22:02 +0300

    commit 21 iunie 2022

--- M01S06/ex05/app.js
+++ M01S06/ex05/app.js
@@ -1,5 +1,61 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    
+// IIFE
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const showButton = document.querySelector('#showButton');
+    const toggleEventButton = document.querySelector('#toggleEventButton');
+    const removeButtons = document.getElementById('removeButtons');
+    const messageClass = 'message';
+    let eventBound = true;
+
+    showButton.addEventListener('click', showAlert);
+
+    showMessage('alerta a fost afisata');
+
+    // hoisting
+
+    function showAlert() {
+      alert('butonul a fost apasat');
+    }
+
+    function showMessage(message) {
+      let paragraphElement = document.querySelector(`.${messageClass}`);
+
+      if (paragraphElement === null) {
+        paragraphElement = document.createElement('p');
+        paragraphElement.classList.add(messageClass);
+
+        document.body.append(paragraphElement);
+      }
+      paragraphElement.innerText = message;
+    }
+
+    toggleEventButton.addEventListener('click', function () {
+      if (eventBound === true) {
+        showButton.removeEventListener('click', showAlert);
+
+        eventBound = false;
+
+        this.innerText = 'Porneste afisarea';
+        showMessage('alerta NU va fi afisata');
+      } else {
+        showButton.addEventListener('click', showAlert);
+
+        eventBound = true;
+
+        this.innerText = 'Opreste afisarea';
+        showMessage('alerta va fi afisata');
+      }
+    });
+
+    removeButtons.addEventListener('click', function () {
+      const removeButtonsConfirmed = confirm('elimina butoane?');
+
+      if (removeButtonsConfirmed === false) {
+        return;
+      }
+      showButton.remove();
+      toggleEventButton.remove();
+    });
   });
 })();
