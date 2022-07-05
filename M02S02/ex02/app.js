484d993da54852217197d9c908e8d7a9262aae27
Fauriis <dobreflorentina58@gmail.com>
2022-07-05 22:31:18 +0300

    jquery

--- M02S02/ex02/app.js
+++ M02S02/ex02/app.js
@@ -11,9 +11,71 @@ $(function () {
       // $detailsFieldset.append(renderPerson(formData));
 
       $form.after(renderData(formData));
+
+      $form.trigger('reset');
+      $('.renderSkillsUl').remove();
     })
     .find('fieldset:nth-child(2)')
     .append(renderSkillControls());
+
+  $('#has-pets').on('click', function () {
+    const $checkbox = $(this);
+    const $nextFieldset = $checkbox.siblings('.pet-fieldset');
+    if ($checkbox.prop('checked')) {
+      $nextFieldset.slideDown();
+    } else $nextFieldset.slideUp();
+  });
+
+  $('.pet-fieldset')
+    .find('button[type="button"]')
+    .on('click', function () {
+      const $petButton = $(this);
+
+      const $inputs = $petButton
+        .parents('.pet-fieldset')
+        .find('input[name^="pet-"]');
+
+      const petValues = [];
+      $inputs.each(function () {
+        const $input = $(this);
+
+        petValues.push($input.val());
+      });
+
+      $petButton.after(renderPetUl(petValues));
+
+      // hoisting - urcate de sistem
+
+      function renderPetUl(petValues) {
+        const petsClassName = 'renderPetUl';
+        let $ul = $(`.${petsClassName}`);
+        if ($ul.length <= 0) {
+          $ul = $('<ul>', {
+            class: petsClassName,
+          });
+        }
+
+        const petData = petValues.join(',');
+
+        const $petLi = $('<li>')
+          .append(
+            $('<span>', {
+              text: petData,
+            }),
+          )
+          .append(
+            $('<input>', {
+              value: petData,
+              type: 'text',
+              name: `pet-data-${petData}`,
+            }),
+          );
+
+        $petLi.appendTo($ul);
+
+        return $ul;
+      }
+    });
 
   //// hoisting doar pentru function
 
@@ -30,11 +92,17 @@ $(function () {
   }
 
   function renderData(formData) {
-    const $container = $('<div>', {
-      class: 'personDisplay',
-    });
+    let $container = $('.personDisplay');
 
-    $container.append(renderPerson(formData)).append(renderSkills(formData));
+    if ($container.length <= 0) {
+      $container = $('<div>', {
+        class: 'personDisplay',
+      });
+    }
+    $container
+      .append(renderPerson(formData))
+      .append(renderSkills(formData))
+      .append(renderPets(formData));
 
     return $container;
   }
@@ -48,15 +116,48 @@ $(function () {
     for (let i = 0; i < keys.length; i++) {
       const keyName = keys[i];
 
-      if (keyName === 'skill-input') {
+      if (keyName === 'skill-input' || !keyName.startsWith('skill-')) {
         continue;
       }
+      // || - SAU
 
       skillsArray.push(objectData[keyName]);
     }
 
+    if (skillsArray.length <= 0) {
+      return '';
+    }
+
     const $p = $('<p>', {
       text: `skills: ${skillsArray}`,
+    });
+
+    return $p;
+  }
+
+  function renderPets(formData) {
+    const iterator = formData.entries();
+    const objectData = Object.fromEntries(iterator);
+
+    const keys = Object.keys(objectData);
+    const petsArray = [];
+
+    for (let i = 0; i < keys.length; i++) {
+      const keyName = keys[i];
+
+      if (!keyName.startsWith('pet-')) {
+        continue;
+      }
+
+      petsArray.push(objectData[keyName]);
+    }
+
+    if (petsArray.length <= 0) {
+      return '';
+    }
+
+    const $p = $('<p>', {
+      text: `Pets: ${petsArray}`,
     });
 
     return $p;
@@ -74,15 +175,52 @@ $(function () {
         });
       }
 
-      $('<li>', {
-        text: skill,
-      })
+      $ul
+        .on('click', '.removeSkillButton', function () {
+          const $removeSkillButton = $(this);
+
+          $removeSkillButton.parent().remove();
+        })
+        .on('click', '.editSkillButton', function () {
+          const $editSkillButton = $(this);
+
+          $editSkillButton
+            .siblings('input[name^="skill-"]')
+            .attr('type', 'text');
+
+          $editSkillButton.siblings('.skillDisplay').hide();
+          $editSkillButton.siblings('.removeSkillButton').hide();
+          $editSkillButton.siblings('.cancelSkillButton').show();
+          $editSkillButton.siblings('.saveSkillButton').show();
+          $editSkillButton.hide();
+        })
+        .on('click', '.cancelSkillButton', function () {
+          const $cancelButton = $(this);
+
+          $cancelButton.hide();
+          $cancelButton
+            .siblings('input[name^="skill-"]')
+            .attr('type', 'hidden');
+          $cancelButton.siblings('.saveSkillButton').hide();
+          $cancelButton.siblings('.skillDisplay').show();
+          $cancelButton.siblings('.removeSkillButton').show();
+          $cancelButton.siblings('.editSkillButton').show();
+        })
+        .on('click', '.saveSkillButton', function () {
+          const $saveButton = $(this);
+
+          $saveButton
+            .siblings('.skillDisplay')
+            .text($saveButton.siblings('input[name^="skill-"]').val());
+
+          // HOMEWORK HERE
+        });
+
+      $('<li>')
         .append(
-          $('<button>', {
-            type: 'button',
-            text: '-',
-            clasa: 'removeSkillButton',
-            title: 'Elimina skill',
+          $('<span>', {
+            text: skill,
+            class: 'skillDisplay',
           }),
         )
         .append(
@@ -92,6 +230,40 @@ $(function () {
             value: skill,
           }),
         )
+        .append(
+          $('<button>', {
+            type: 'button',
+            text: '-',
+            class: 'removeSkillButton',
+            title: 'Elimina skill',
+          }),
+        )
+
+        .append(
+          $('<button>', {
+            type: 'button',
+            text: 'editeaza',
+            class: 'editSkillButton',
+            title: 'Editeaza skill',
+          }),
+        )
+        .append(
+          $('<button>', {
+            type: 'button',
+            text: 'Salveaza',
+            class: 'saveSkillButton',
+            title: 'Salveaza skill',
+          }).hide(),
+        )
+        .append(
+          $('<button>', {
+            type: 'button',
+            text: 'Anuleaza',
+            class: 'cancelSkillButton',
+            title: 'Anuleaza',
+          }).hide(),
+        )
+
         .appendTo($ul);
 
       return $ul;
