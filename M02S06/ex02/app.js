453a1cb1e57b7bb6b3adbdd32a3bb067bf5191e0
Fauriis <dobreflorentina58@gmail.com>
2022-07-21 22:08:03 +0300

    java

--- M02S06/ex02/app.js
+++ M02S06/ex02/app.js
@@ -1,9 +1,17 @@
+// sintactic sugar
+const [a, b, ...parametres] = process.argv;
 
+// normal
 
+// const c = process.argv[0];
+// const d = process.argv[1];
+// const params = process.argv.slice(2);
 
+console.log(parametres);
 
+const width = parametres[0];
+const height = parametres[1];
 
+// const[width height, ,,,,last] = parametres
 
-
-
-  
+console.log(width * height);
---
+++ M02S06/ex03/app.js
@@ -0,0 +1,11 @@
+// old import common js
+
+const fs = require('fs');
+
+// Creaza in director un script node care sa citeasca informatii din fisierul file.txt si sa le afiseze in consola
+
+const data = fs.readFileSync('./file.txt', 'utf-8');
+
+console.log(data);
+
+// Creaza in director un alt script node care sa citeasca informatii din fisierul file.txt, sa adauge textului citit propozitia: “This is our node script!”. In final, afiseaza rezultatul in consola.
